dc0f79fe265f8e33ce99fc29b7f59e16b7968f07
LULU-LUX <121755414+LULU-LUX@users.noreply.github.com>
2026-02-06 22:03:19 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'ClearSwap',
+  description: 'DEX na ARC Testnet',
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body>{children}</body>
     </html>
   )
